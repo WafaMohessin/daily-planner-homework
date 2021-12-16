8885a007ce0d6d70930dc7375b9a512180f5d5a9
Wafa Mohessin <wafamohessin@gmail.com>
2021-12-15 21:55:16 -0500

    added screenshot into README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # daily-planner-homework
+This app allows you to track your tasks throughout the day.
+
+# ScreenShot
+![Text](./image/screenShot.png)
+# How to use this app
+1. Enter your activity that you want to complete. 
+2. Save your activity by clicking the save button in the right of the block. 
+3. Happy daily activity!
+
+# Features
+- Program show your daily activities in the past, present or future
+- Yout activitis will be saved into local storage so that you can keep coming back to it.
+
+# Screenshot
+
--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 // Declaring Variables
-let currentDayDisplay = $('#currentDay')
+var currentDisplay = $('#currentDisplay')
+
 // Display time when page loads
-currentDayDisplay.text(moment().format('LLLL'))
-let currentHour = moment().hour()
-let timeBlocks = []
+currentDisplay.text(moment().format('LLLL'))
+var currentHour = moment().hour()
+var timeBlocks = []
 
 
+// hourBlock Object
+class hourBlock {
+    constructor(time, taskEl, buttonEl) {
+        this.time = time
+        this.taskEl = taskEl
+        this.buttonEl = buttonEl
+    }
+    // load activity into the textareas
+    loadEvents() {
+        this.taskEl.val(localStorage.getItem(this.time))
+    }
+    // Saves the activities into local data
+    saveButton() {
+        this.buttonEl.on('click', e => {
+            var task = $(e.target).closest('div.row').find('textarea').val()
+            localStorage.setItem(this.time, task)
+        })
+    }
+}
 
+// Updates time every second
+setInterval(function () {
+    currentDisplay.text(moment().format('LLLL'))
+    currentHour = moment().hour()
+}, 1000)
 
+// timeBlock objects and sets the colors of the textareas
+    $('.time-block').each(function(i) {
+
+    timeBlocks.push(new hourBlock((i + 9), $(this).find('textarea'), $(this).find('.saveBtn')))
+    timeBlocks[i].loadEvents()
+    timeBlocks[i].saveButton()
+    if (moment(currentHour).isSame(timeBlocks[i].time)) {
+
+        $(this).find('textarea').toggleClass('past current')
+
+    } else if (moment(currentHour).isBefore(timeBlocks[i].time)) {
+
+        $(this).find('textarea').toggleClass('past current')
 
     }
 
-})s
+})
